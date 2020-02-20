0ace8cbc0e4036892a21b3628c2edccf4237a3e8
punzar <punzar73@gmail.com>
2020-02-21 03:58:09 +0400

    column and row

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(
-      MaterialApp(
-          home: Scaffold(
-              backgroundColor: Colors.amber,
-              appBar: AppBar(
-                title: Text("This is the way"),
-                backgroundColor: Colors.indigo[100],
-              ),
-          body: Center(
-            child: Image(
-              image: NetworkImage('https://www.tokkoro.com/picsup/3206880-landscape-nature-river-waterfall-waterfalls.jpg'),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: 100,
+              color: Colors.red,
             ),
-          ),)),
-    );
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellowAccent
+                ),
+                Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellowAccent.shade400,
+                )
+              ],
+            ),
+            Container(
+              width: 100,
+              color: Colors.blue,
+            )
+
+          ],
+        ),
+      ),
+    ));
+  }
+}
